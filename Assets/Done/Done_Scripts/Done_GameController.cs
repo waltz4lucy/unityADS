75e3acd08030b9b3a43d186551247004d048ccff
Lucy Diamond <waltz4lucy@gmail.com>
2016-08-13 04:55:19 +0900

    init

---
+++ Assets/Done/Done_Scripts/Done_GameController.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using UnityEngine.Advertisements;
+using System.Collections;
+
+public class Done_GameController : MonoBehaviour
+{
+	public GameObject[] hazards;
+	public Vector3 spawnValues;
+	public int hazardCount;
+	public float spawnWait;
+	public float startWait;
+	public float waveWait;
+	
+	public GUIText scoreText;
+	public GUIText gameOverText;
+	
+	private bool gameOver;
+	private int score;
+	
+	void Start ()
+	{
+		gameOver = false;
+		gameOverText.text = "";
+		score = 0;
+		UpdateScore ();
+		StartCoroutine (SpawnWaves ());
+	}
+	
+	void Update ()
+	{
+		if (gameOver)
+		{
+			gameOver = false;
+			Invoke ("ShowAd", waveWait);
+		}
+	}
+	
+	IEnumerator SpawnWaves ()
+	{
+		yield return new WaitForSeconds (startWait);
+
+		while (true)
+		{
+			for (int i = 0; i < hazardCount; i++)
+			{
+				GameObject hazard = hazards [Random.Range (0, hazards.Length)];
+				Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+				Quaternion spawnRotation = Quaternion.identity;
+				Instantiate (hazard, spawnPosition, spawnRotation);
+				yield return new WaitForSeconds (spawnWait);
+			}
+
+			yield return new WaitForSeconds (waveWait);
+		}
+	}
+	
+	public void AddScore (int newScoreValue)
+	{
+		score += newScoreValue;
+		UpdateScore ();
+	}
+	
+	void UpdateScore ()
+	{
+		scoreText.text = "Score: " + score;
+	}
+	
+	public void GameOver ()
+	{
+		gameOverText.text = "Game Over!";
+		gameOver = true;
+	}
+
+	public void ShowAd ()
+	{
+		if (Advertisement.IsReady())
+		{
+			ShowOptions options = new ShowOptions();
+			options.resultCallback = HandleShowResult;
+
+			Advertisement.Show(null, options);
+		}
+	}
+
+	private void HandleShowResult (ShowResult result)
+	{
+		switch (result)
+		{
+		case ShowResult.Finished:
+			UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex);
+			break;
+		case ShowResult.Skipped:
+			UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex);
+			break;
+		case ShowResult.Failed:
+			UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex);
+			break;
+		}
+	}
+}
---
+++ Assets/Done/Done_Scripts/Done_PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Done_Boundary 
+{
+	public float xMin, xMax, zMin, zMax;
+}
+
+public class Done_PlayerController : MonoBehaviour
+{
+	public float speed;
+	public float tilt;
+	public Done_Boundary boundary;
+
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+	 
+	private float nextFire;
+	
+	void Update ()
+	{
+//		if (Input.GetButton("Fire1") && Time.time > nextFire) 
+		if (Time.time > nextFire)
+		{
+			nextFire = Time.time + fireRate;
+			Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+			GetComponent<AudioSource>().Play ();
+		}
+	}
+
+	void FixedUpdate ()
+	{
+		if (Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Moved) {
+			Vector2 touchDeltaPosition = Input.GetTouch (0).deltaPosition;
+			float moveHorizontal = touchDeltaPosition.x * 0.1f;
+			float moveVertical = touchDeltaPosition.y * 0.1f;
+
+			Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+			GetComponent<Rigidbody>().velocity = movement * speed;
+		} else {
+			float moveHorizontal = Input.GetAxis ("Horizontal");
+			float moveVertical = Input.GetAxis ("Vertical");
+
+			Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+			GetComponent<Rigidbody>().velocity = movement * speed;
+		}
+		
+		GetComponent<Rigidbody>().position = new Vector3
+		(
+			Mathf.Clamp (GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax), 
+			0.0f, 
+			Mathf.Clamp (GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
+		);
+		
+		GetComponent<Rigidbody>().rotation = Quaternion.Euler (0.0f, 0.0f, GetComponent<Rigidbody>().velocity.x * -tilt);
+	}
+}
